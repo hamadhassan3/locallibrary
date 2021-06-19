0022b1d1198146fada4095fbcd76d3062d4d4f69
hammad hassan <hammadhassan@pk.folio3.com>
2021-06-19 13:44:32 +0500

    Added the home page

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,3 +4,9 @@ from . import views
 urlpatterns = [
 
 ]
+
+
+# The home page address is added here
+urlpatterns += [
+    path('', views.index, name='index'),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Language, Genre, Author
 
-# Create your views here.
+
+
+def index(request):
+    """This function handles the request for home page
+    The home page displays the count of all items in library
+    """
+
+    # Storing the count of each type of item
+    
+    count_of_books = Book.objects.all().count()
+    count_of_authors = Author.objects.all().count()
+    count_of_genres = Genre.objects.all().count()
+    count_of_languages = Language.objects.all().count()
+    count_of_bookinstance = BookInstance.objects.all().count()
+
+    # Storing available books count
+
+    count_of_available_books = BookInstance.objects.all().filter(status__exact='a').count()
+
+    # The count of each item is stored in the context so that the page can be populated
+
+    context = {
+        'count_of_books': count_of_books,
+        'count_of_authors': count_of_authors,
+        'count_of_genres': count_of_genres,
+        'count_of_languages': count_of_languages,
+        'count_of_bookinstances': count_of_bookinstance,
+    }
+
+
+    return render(request, 'index.html', context)
