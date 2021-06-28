4eb03b720fff5c1218e4267d1a020ca218c9e907
hammad hassan <hammadhassan@pk.folio3.com>
2021-06-28 17:49:16 +0500

    Setting up heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['powerful-sierra-51864.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['powerful-sierra-51864.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
